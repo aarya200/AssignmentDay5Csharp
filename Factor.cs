314e59676768258f0c9b7fc3085f749f2b5cc026
Aarya Khanapurkar <124122792+aarya200@users.noreply.github.com>
2023-04-17 00:34:47 +0530

    Add files via upload

---
+++ Factor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeFactorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find its prime factors:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Prime factors of {n} are:");
+            PrimeFactorization(n);
+
+            Console.ReadLine();
+        }
+
+        static void PrimeFactorization(int n)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.Write($"{i} ");
+                    n = n / i;
+                }
+            }
+        }
+    }
+}
