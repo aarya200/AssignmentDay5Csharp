99da71948f8c43559c01f5efafe92a7c4dcd92d9
Aarya Khanapurkar <124122792+aarya200@users.noreply.github.com>
2023-04-17 00:34:14 +0530

    Add files via upload

---
+++ EvenOrodd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5problems
+{
+    class FindEvenOdd
+    {
+        public void evenOrOdd()
+        {
+            Console.WriteLine(" Ente a number ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + "Number is a Even Number");
+            }
+            else
+            {
+                Console.WriteLine(num + "Number is a Odd Number");
+            }
+
+        }
+    }
+}
